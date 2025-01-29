d7a8c5b7811759e34b79f88bc5e62aa510f45bf7
vitordrager1 <vitordrager09@gmail.com>
2025-01-29 17:36:08 -0300

    firebase config

--- plan-poker/services/firebase.tsx
+++ plan-poker/services/firebase.tsx
@@ -13,15 +13,15 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "poker-plan-br.firebaseapp.com",
-  projectId: "poker-plan-br",
-  storageBucket: "poker-plan-br.firebasestorage.app",
-  messagingSenderId: "180496378322",
-  appId: "1:180496378322:web:d604f5e2075a73adcbc00b",
-  measurementId: "G-SQHV0BLETP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-console.log("Firebase API Key:", process.env.FIREBASE_API_KEY);
+
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // //const analytics = getAnalytics(app);
